e3ba14428021d608f598ef43c77cb4e0ac44ce22
Dobravoski <lipedobravoski@gmail.com>
2026-03-22 23:51:40 -0300

    Getting data from database

---
+++ jdbc-mysql/src/GettingDataFromDataBase.java
@@ -0,0 +1,23 @@
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class GettingDataFromDataBase {
+    static void main() {
+        try (Connection conn = DB.getConnection();
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from seller")) {
+
+            while(resultSet.next()) {
+                System.out.printf("%d - %s - %s\n", resultSet.getInt("Id"), resultSet.getString("Name"), resultSet.getString("Email"));
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+}
--- jdbc-mysql/src/db/DbException.java
+++ jdbc-mysql/src/db/DbException.java
@@ -1,7 +1,7 @@
 package db;
 
 public class DbException extends RuntimeException{
-    DbException(String message){
+    public DbException(String message){
         super(message);
     }
 }
